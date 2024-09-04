d5c034eaf07708bf1f57bd66cadc907a5ebf7e9a
VaibhavRai01 <122098758+VaibhavRai01@users.noreply.github.com>
2024-09-04 15:13:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -169,4 +169,4 @@ def upload_files():
 
 # Main function to run the app
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000,debug=True)
